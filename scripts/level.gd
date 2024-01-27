656c7931fc964951f9ba4076dea34041997a2797
Juanfrcaliz <jkramos91@gmail.com>
2024-01-27 20:17:59 +0100

    add level flag

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,6 +1,6 @@
 extends Node
 
-
+var level_number = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -14,7 +14,21 @@ func _process(delta):
 func _on_box_dog_entered():
 	$Pug.change_mobility(false)
 	$Pug.show_ball()
+	transitionLevel()
 
 
 func _on_box_dog_exited():
 	$Pug.change_mobility(true)
+	
+func transitionLevel():
+	if level_number == 0:
+		var tween = get_tree().create_tween()
+		tween.tween_property($CanvasLayer/ColorRect, "color:a", 1, 1).set_trans(Tween.TRANS_SINE)
+		$AudioStreamPlayer2D.play()
+		level_number += 1
+
+
+func _on_audio_stream_player_2d_finished():
+	print('terminao')
+	var tween = get_tree().create_tween()
+	tween.tween_property($CanvasLayer/ColorRect, "color:a", 0, 1).set_trans(Tween.TRANS_SINE)
