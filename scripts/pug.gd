ef49077a44ae0677c4ed9d590ecbcb9b1ae25d0c
Raulios <raulmolina88@gmail.com>
2024-01-27 17:51:26 +0100

    solved merging errors

--- scripts/pug.gd
+++ scripts/pug.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+const PUSH_FORCE = 30.0
+var can_push = true
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
